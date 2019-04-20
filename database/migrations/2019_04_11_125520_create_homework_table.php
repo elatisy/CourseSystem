105590c9713a1e292b452ce61a554fe5e8f00a46
elatisy <elatisy@outlook.com>
2019-04-20 15:18:59 +0800

    debug

--- database/migrations/2019_04_11_125520_create_homework_table.php
+++ database/migrations/2019_04_11_125520_create_homework_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateHomeworksTable extends Migration
+class CreateHomeworkTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2019_04_11_125542_create_homework_data_table.php
+++ database/migrations/2019_04_11_125542_create_homework_data_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateHomeworkDatasTable extends Migration
+class CreateHomeworkDataTable extends Migration
 {
     /**
      * Run the migrations.
